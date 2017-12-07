afca56113801b1b5066e77aa108330282f56aeb9
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2017-12-07 18:16:33 +1100

    Change navbar-brand to navbar-nav (Eureka)

--- src/menu/menu.jsx
+++ src/menu/menu.jsx
@@ -8,7 +8,7 @@ export default class Menu extends React.Component {
         return (
             <div className="row" id="menu">
                 <nav className="navbar navbar-light bg-dark navbar-expand-md" id="nav_menu" role="navigation" >
-                    <a href="#menu" className="navbar-brand nav-link nav_menu-link text-white justify-content-start">Eureka 22</a>    
+                    <a href="#menu" className="navbar-nav nav-link nav_menu-link text-white justify-content-start">Eureka 22</a>    
                     <a href="#menu" className="navbar-nav nav-link nav_menu-link text-white">Updates</a>
                     <a href="#menu" className="navbar-nav nav-link nav_menu-link text-white ">Photos</a>
                         <a href="#menu" className="navbar-nav nav-link nav_menu-link text-white ">Plans</a>
