6f318d28dcfed51e13cf58312e1de475cbc160c9
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2017-12-08 10:55:29 +1100

    Add multiple entry points to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: __dirname + '/src/index.html',
-    filename: 'index.html',
-    inject: 'body'
-})
+let entry = {
+    index: './src/index.jsx',
+    updates: "./src/updates/updates.jsx",
+    photos: "./src/photos/photos.jsx",
+};
+let entryHtmlPlugins = Object.keys(entry).map(function (entryName) {
+    return new HtmlWebpackPlugin({
+        inject: 'body',
+        title: entryName,
+        template: './src/index.html',
+        filename:  entryName + ".html",
+        chunks: [entryName]
+    })
+});
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    entry: entry,
     output: {
         path: __dirname + '/docs',
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     resolve: {
         extensions: ['.js', '.jsx']
@@ -29,6 +38,10 @@ module.exports = {
                 test: /\.css$/,
                 loaders: ['style-loader', 'css-loader'],
             },
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader'
+                },
 
     //         {
     //             test: /\.(jpe?g|png|gif|svg)$/i,
@@ -46,7 +59,7 @@ module.exports = {
             }
         ]
     },
-    plugins: [HtmlWebpackPluginConfig]
+    plugins: entryHtmlPlugins
 };
 
 
